4d0223eecf9c2acd84513a019b84ab30ba2a4801
SalmanRaju <salmanrajup167@gmail.com>
2026-04-15 12:31:06 +0530

    changes accepted

--- ecommerce/ecommerce-frontend/src/App.js
+++ ecommerce/ecommerce-frontend/src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login";
-import Products from "./Products.js";
+import Products from "./Products";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* IMPORTANT FIX */}
+        <Route path="/" element={<Login />} />
+
         <Route path="/login" element={<Login />} />
         <Route path="/products" element={<Products />} />
       </Routes>
