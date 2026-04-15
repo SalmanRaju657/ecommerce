aff11b0b20c4858d1965db7c8df72ba47187abc9
SalmanRaju <salmanrajup167@gmail.com>
2026-04-15 12:59:26 +0530

    finally work

--- ecommerce/ecommerce-frontend/src/App.js
+++ ecommerce/ecommerce-frontend/src/App.js
@@ -3,16 +3,19 @@ import Login from "./Login";
 import Products from "./Products";
 
 function App() {
+  const token = localStorage.getItem("token");
+
   return (
     <BrowserRouter>
       <Routes>
-
-        {/* Redirect root */}
         <Route path="/" element={<Navigate to="/login" />} />
 
         <Route path="/login" element={<Login />} />
-        <Route path="/products" element={<Products />} />
 
+        <Route
+          path="/products"
+          element={token ? <Products /> : <Navigate to="/login" />}
+        />
       </Routes>
     </BrowserRouter>
   );
