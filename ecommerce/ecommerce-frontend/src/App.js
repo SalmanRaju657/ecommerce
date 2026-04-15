2ced25f9c646dcca0e7f26ac028374647a40c63a
SalmanRaju <salmanrajup167@gmail.com>
2026-04-15 12:24:21 +0530

    added vercel config

--- ecommerce/ecommerce-frontend/src/App.js
+++ ecommerce/ecommerce-frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Products />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </BrowserRouter>
   );
